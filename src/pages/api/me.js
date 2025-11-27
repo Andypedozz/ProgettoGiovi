6c10fe4b50b748cc765548032904650d080b44dc
andrea <andypedini02@gmail.com>
2025-11-27 14:35:07 +0100

    .

--- src/pages/api/me.js
+++
@@ -1,9 +0,0 @@
-// src/pages/api/me.js
-import { authRequired } from "../../middleware.js";
-
-export const GET = authRequired(async ({ user }) => {
-  return new Response(JSON.stringify({ user }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-});
